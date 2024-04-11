54241f1846a01a2efeb83af0efc6b53bbfe6f59f
Edy Thawne <itsramires@gmail.com>
2024-04-10 22:11:49 -0600

    Create kde6.sh

---
+++ kde6.sh
@@ -0,0 +1,67 @@
+#!/bin/env bash
+
+echo "Installing the needed packages."
+sudo dnf install -y --setopt=install_weak_deps=False\
+@"Hardware Support" @Fonts\
+ark \
+bluedevil \
+breeze-gtk \
+breeze-icon-theme \
+colord-kde \
+dolphin \
+glibc-all-langpacks \
+gnome-keyring-pam \
+intel-media-driver \
+libva-utils libvdpau-va-gl \
+kde-gtk-config \
+kde-partitionmanager \
+kde-style-breeze \
+kdegraphics-thumbnailers \
+kdeplasma-addons \
+kdialog \
+kdnssd \
+kf5-baloo-file \
+kf5-kipi-plugins \
+kf5-kwayland \
+khotkeys \
+kmenuedit \
+konsole \
+kscreen \
+kscreenlocker \
+ksshaskpass \
+kinfocenter \
+kwin-wayland kwrite \
+libinput \
+libwayland-* \
+mesa-dri-drivers mesa-va-drivers \
+NetworkManager-config-connectivity-fedora \
+pam-kwallet \
+phonon-qt5-backend-gstreamer \
+pinentry-qt \
+plasma-breeze \
+plasma-desktop \
+plasma-desktop-doc \
+plasma-nm \
+plasma-pa \
+plasma-systemmonitor \
+plasma-workspace-wallpapers \
+plasma-workspace-wayland \
+polkit-kde \
+qt5-qtbase-gui \
+qt5-qtdeclarative \
+qt6-qtwayland \
+sddm-breeze \
+sddm-kcm \
+sddm-wayland-plasma \
+sni-qt \
+vulkan \
+wayland-utils \
+xorg-x11-server-Xwayland \
+xwaylandvideobridge
+iwlwifi-mvm-firmware
+
+
+# enable SDDM
+echo "Enabling the graphical login."
+sudo systemctl enable --now sddm && \
+sudo systemctl set-default graphical.target
