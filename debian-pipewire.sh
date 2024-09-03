07bc58b9cdb2592283519c044261a67aba68a353
Edy Thawne <itsramires@gmail.com>
2024-09-03 05:22:30 +0000

    Update debian-pipewire.sh

--- debian-pipewire.sh
+++ debian-pipewire.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
 pulseaudio --version
+systemctl --user --now disable pulseaudio.service pulseaudio.socket
+systemctl --user mask pulseaudio
+
+sudo apt install wireplumber 
+systemctl --user --now enable wireplumber.service
+
+sudo apt install bluez  libspa-0.2-bluetooth
+sudo apt install bluez  libspa-0.2-bluetooth pipewire-pulse pipewire-audio-client-libraries
+systemctl --user restart pipewire pipewire-pulse wireplumber
