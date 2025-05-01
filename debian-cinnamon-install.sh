a16309b985e8dd7b16bb975d8ab4a550bba7640c
Edy Thawne <itsramires@gmail.com>
2025-05-01 08:31:55 -0600

    Update debian-cinnamon-install.sh

--- debian-cinnamon-install.sh
+++ debian-cinnamon-install.sh
@@ -6,16 +6,17 @@ sudo apt upgrade -y
 
 # Lista de paquetes incluidos en gnome-core excepto gnome-software
 PACKAGES=(
-    cinnamon
+    cinnamon-core
     desktop-base
     slick-greeter
     lightdm
 )
 
 # Instalar los paquetes
-sudo apt install -y "${PACKAGES[@]}"
+sudo apt install -y -q --no-install-recommends "${PACKAGES[@]}"
 
-sudo systemctl enable gdm
+sudo dpkg-reconfigure lightdm
+sudo systemctl enable lightdm
 sudo systemctl set-default graphical.target
 
 # Fin del script
