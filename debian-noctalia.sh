5e84af4b76d0f46478500a81c02bab13a0f2047a
Edy Thawne <itsramires@gmail.com>
2026-05-10 21:55:56 -0600

    Update debian-noctalia.sh

--- debian-noctalia.sh
+++ debian-noctalia.sh
@@ -8,4 +8,10 @@ curl -fsSL https://pkg.noctalia.dev/gpg.key | sudo gpg --dearmor -o /etc/apt/key
 echo "Añadiendo repositorio de Noctalia..."
 echo "deb [signed-by=/etc/apt/keyrings/noctalia.gpg] https://pkg.noctalia.dev/apt trixie main" | sudo tee /etc/apt/sources.list.d/noctalia.list
 
+echo "Añadiendo repositorio de Dank para instalar niri..."
+curl -fsSL https://download.opensuse.org/repositories/home:AvengeMedia:danklinux/Debian_13/Release.key | \
+  sudo gpg --dearmor -o /etc/apt/keyrings/danklinux.gpg
+echo "deb [signed-by=/etc/apt/keyrings/danklinux.gpg] https://download.opensuse.org/repositories/home:/AvengeMedia:/danklinux/Debian_13/ /" | \
+  sudo tee /etc/apt/sources.list.d/danklinux.list
+
 sudo apt update
